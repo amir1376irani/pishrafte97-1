7975aed0618006dcbdca987c95e80ab605a553b4
amir1376irani <amir1376irani@yahoo.com>
2019-02-12 04:57:57 +0330

    2

--- exam/staticClass.cpp
+++ exam/staticClass.cpp
@@ -4,12 +4,17 @@ using namespace std;
 class A
 {
 public:
-	static int min (int a, int b){return a<b?a:b;}
+	static int min(int a, int b){return a<b?a:b;}
 };
+
+
+/*namespace A
+{
+	int min(int a, int b){return a<b?a:b;}
+};*/
 
 int main(void)
 {
 	cout << A::min(105,250) << endl;// 105
 	return 0;
 }
-
